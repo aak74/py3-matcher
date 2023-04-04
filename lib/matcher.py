b5a8e31a00f9647e32caac86cf9f9015835b1330
Андрей Копылов <riu@inbox.ru>
2023-04-04 19:03:03 +0300

    added tests

--- lib/matcher.py
+++ lib/matcher.py
@@ -5,6 +5,10 @@ class Matcher:
         'test',
         'testing',
     ]
+    SUFFIXES = [
+        'az1',
+        'az2',
+    ]
     __new_projects = []
     __binds = None
 
@@ -15,16 +19,63 @@ class Matcher:
             self.__binds[project] = []
 
         for service in services:
-            if service in self.__binds:
-                self.add_bind(service, service)
+            if self.is_valid_with_prefix_and_suffix(service):
                 continue
 
-            parts = service.split('_', 1)
-            if parts[0] in self.PREFIXES:
-                self.add_bind(parts[1], service)
+            if self.is_valid_direct(service):
+                continue
+
+            if self.is_valid_with_prefix(service):
+                continue
+
+            if self.is_valid_with_suffix(service):
                 continue
 
             self.add_bind(service, service)
+
+    def is_valid_direct(self, service: str):
+        if service in self.__binds:
+            self.add_bind(service, service)
+            return True
+
+        return False
+
+    def is_valid_with_prefix(self, service: str):
+        parts = service.split('_', 1)
+        if len(parts) < 2:
+            return False
+
+        if parts[0] in self.PREFIXES:
+            self.add_bind(parts[1], service)
+            return True
+
+        return False
+
+    def is_valid_with_suffix(self, service: str):
+        parts = service.rsplit('_', 1)
+        if len(parts) < 2:
+            return False
+
+        if parts[1] in self.SUFFIXES:
+            self.add_bind(parts[0], service)
+            return True
+
+        return False
+
+    def is_valid_with_prefix_and_suffix(self, service: str):
+        parts_prefix = service.split('_', 1)
+        if len(parts_prefix) < 2:
+            return False
+
+        parts_suffix = parts_prefix[1].rsplit('_', 1)
+        if len(parts_suffix) < 2:
+            return False
+
+        if parts_suffix[1] in self.SUFFIXES:
+            self.add_bind(parts_suffix[0], service)
+            return True
+
+        return False
 
     def add_bind(self, project: str, service: str):
         if project not in self.__binds:
--- tests/matcher_test.py
+++ tests/matcher_test.py
@@ -4,24 +4,6 @@ from lib.matcher import Matcher
 
 
 class MatcherTestCase(unittest.TestCase):
-    def test_prefixes(self):
-        projects = ['front']
-        services = ['prod_front']
-        matcher = Matcher()
-
-        matcher.match(projects, services)
-        self.assertEqual([], matcher.get_new_projects())
-        self.assertEqual({'front': ['prod_front']}, matcher.get_binds())
-
-    def test_no_prefixes(self):
-        projects = ['front']
-        services = ['front']
-        matcher = Matcher()
-
-        matcher.match(projects, services)
-        self.assertEqual([], matcher.get_new_projects())
-        self.assertEqual({'front': ['front']}, matcher.get_binds())
-
     def test_new_project(self):
         projects = []
         services = ['front']
@@ -31,6 +13,78 @@ class MatcherTestCase(unittest.TestCase):
         print(matcher.get_binds())
         self.assertEqual(['front'], matcher.get_new_projects())
         self.assertEqual({'front': ['front']}, matcher.get_binds())
+
+    def test_new_projects_wuth_prefixes(self):
+        projects = []
+        services = ['prod_front', 'test_back']
+        matcher = Matcher()
+
+        matcher.match(projects, services)
+        print(matcher.get_binds())
+        self.assertEqual(['front', 'back'], matcher.get_new_projects())
+        expected_binds = {
+            'front': ['prod_front'],
+            'back': ['test_back']
+        }
+        self.assertEqual(expected_binds, matcher.get_binds())
+
+    def test_no_prefixes_wo_new_project(self):
+        projects = ['front']
+        services = ['front']
+        matcher = Matcher()
+
+        matcher.match(projects, services)
+        self.assertEqual([], matcher.get_new_projects())
+        self.assertEqual({'front': ['front']}, matcher.get_binds())
+
+    def test_prefixes_wo_new_project(self):
+        projects = ['front']
+        services = ['prod_front']
+        matcher = Matcher()
+
+        matcher.match(projects, services)
+        self.assertEqual([], matcher.get_new_projects())
+        self.assertEqual({'front': ['prod_front']}, matcher.get_binds())
+
+    def test_prefixes_two_projects(self):
+        projects = ['front', 'back']
+        services = ['prod_front', 'test_front', 'prod_back', 'test_back']
+        matcher = Matcher()
+
+        matcher.match(projects, services)
+        self.assertEqual([], matcher.get_new_projects())
+        expected_binds = {
+            'front': ['prod_front', 'test_front'],
+            'back': ['prod_back', 'test_back']
+        }
+        self.assertEqual(expected_binds, matcher.get_binds())
+
+    def test_suffixes_wo_new_project(self):
+        projects = ['front']
+        services = ['front_az1']
+        matcher = Matcher()
+
+        matcher.match(projects, services)
+        self.assertEqual([], matcher.get_new_projects())
+        self.assertEqual({'front': ['front_az1']}, matcher.get_binds())
+
+    def test_prefixes_and_suffixes_wo_new_project(self):
+        projects = ['front']
+        services = ['prod_front_az1']
+        matcher = Matcher()
+
+        matcher.match(projects, services)
+        self.assertEqual([], matcher.get_new_projects())
+        self.assertEqual({'front': ['prod_front_az1']}, matcher.get_binds())
+
+    def test_prefixes_and_suffixes_w_new_project(self):
+        projects = []
+        services = ['prod_front_aaa']
+        matcher = Matcher()
+
+        matcher.match(projects, services)
+        self.assertEqual(['front_aaa'], matcher.get_new_projects())
+        self.assertEqual({'front_aaa': ['prod_front_aaa']}, matcher.get_binds())
 
 
 if __name__ == '__main__':
